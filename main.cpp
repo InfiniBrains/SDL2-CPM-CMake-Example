33839ce2eb8aebb5220e9f96fba30b5bbdcc69f2
Alexandre Tolstenko <tolstenko@gmail.com>
2022-11-09 16:05:14 -0500

    feat: small example to show how to use cmake cpm and sdl2

---
+++ main.cpp
@@ -0,0 +1,112 @@
+#define SDL_MAIN_HANDLED true
+#include <iostream>
+#include <algorithm>
+
+#include <SDL.h>
+
+#define SCREEN_WIDTH    800
+#define SCREEN_HEIGHT   600
+
+int main(int argc, char* argv[])
+{
+    // Unused argc, argv
+    (void) argc;
+    (void) argv;
+
+    // Initialize SDL
+    if(SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        std::cout << "SDL could not be initialized!" << std::endl
+                  << "SDL_Error: " << SDL_GetError() << std::endl;
+        return 0;
+    }
+
+#if defined linux && SDL_VERSION_ATLEAST(2, 0, 8)
+    // Disable compositor bypass
+    if(!SDL_SetHint(SDL_HINT_VIDEO_X11_NET_WM_BYPASS_COMPOSITOR, "0"))
+    {
+        std::cout << "SDL can not disable compositor bypass!" << std::endl;
+        return 0;
+    }
+#endif
+
+    // Create window
+    SDL_Window *window = SDL_CreateWindow("Basic C++ SDL project",
+                                          SDL_WINDOWPOS_UNDEFINED,
+                                          SDL_WINDOWPOS_UNDEFINED,
+                                          SCREEN_WIDTH, SCREEN_HEIGHT,
+                                          SDL_WINDOW_SHOWN);
+    if(!window)
+    {
+        std::cout << "Window could not be created!" << std::endl
+                  << "SDL_Error: " << SDL_GetError() << std::endl;
+    }
+    else
+    {
+        // Create renderer
+        SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+        if(!renderer)
+        {
+            std::cout << "Renderer could not be created!" << std::endl
+                      << "SDL_Error: " << SDL_GetError() << std::endl;
+        }
+        else
+        {
+            // Declare rect of square
+            SDL_Rect squareRect;
+
+            // Square dimensions: Half of the min(SCREEN_WIDTH, SCREEN_HEIGHT)
+            squareRect.w = std::min(SCREEN_WIDTH, SCREEN_HEIGHT) / 2;
+            squareRect.h = std::min(SCREEN_WIDTH, SCREEN_HEIGHT) / 2;
+
+            // Square position: In the middle of the screen
+            squareRect.x = SCREEN_WIDTH / 2 - squareRect.w / 2;
+            squareRect.y = SCREEN_HEIGHT / 2 - squareRect.h / 2;
+
+
+            // Event loop exit flag
+            bool quit = false;
+
+            // Event loop
+            while(!quit)
+            {
+                SDL_Event e;
+
+                // Wait indefinitely for the next available event
+                SDL_WaitEvent(&e);
+
+                // User requests quit
+                if(e.type == SDL_QUIT)
+                {
+                    quit = true;
+                }
+
+                // Initialize renderer color white for the background
+                SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+
+                // Clear screen
+                SDL_RenderClear(renderer);
+
+                // Set renderer color red to draw the square
+                SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, 0x00, 0xFF);
+
+                // Draw filled square
+                SDL_RenderFillRect(renderer, &squareRect);
+
+                // Update screen
+                SDL_RenderPresent(renderer);
+            }
+
+            // Destroy renderer
+            SDL_DestroyRenderer(renderer);
+        }
+
+        // Destroy window
+        SDL_DestroyWindow(window);
+    }
+
+    // Quit SDL
+    SDL_Quit();
+
+    return 0;
+}
